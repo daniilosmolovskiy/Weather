7dd5508b2c4eeb81d39a43175f0667e4e782f089
Daniil Osmolovskiy <danielosmolovskiy@gmail.com>
2020-09-08 13:27:55 +0300

    Heroku command added

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,12 +5,15 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
-  mode: 'universal',
+  mode: 'spa',
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  router: {
+    base: '/weather/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
